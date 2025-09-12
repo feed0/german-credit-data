17261d03da42879c854d3a3cd11fc47c8b20b385
Felipe O. <97348832+feed0@users.noreply.github.com>
2025-09-12 20:53:35 +0000

    [GCD] - FAYAAD 1 - Selection: removed categorical dtype for now

--- python/src/tests/table_formatting_test.py
+++ python/src/tests/table_formatting_test.py
@@ -4,8 +4,6 @@ import pandas as pd
 class TableFormattingTest:
     '''Ensures column statistics are unchanged after processing'''
     
-    ORDINAL_COLUMN = ('A8', 'Installment rate in percentage of disposable income')
-
     def test_column_statistics(self, original: pd.DataFrame, processed: pd.DataFrame):
         # 1. Prepare
         original = original.copy()
@@ -16,33 +14,26 @@ class TableFormattingTest:
 
         # 2. Reverse the boolean columns mapping in processed
         inverse_boolean_map = {
-            ('A19', 'Telephone')        : {True: 'A192', False: 'A191'},
-            ('A20', 'Foreign worker')   : {True: 'A201', False: 'A202'},
-            ('T1', 'Is good credit')    : {True: 1, False: 2}
+            'A19 - Telephone'        : {True: 'A192', False: 'A191'},
+            'A20 - Foreign worker'   : {True: 'A201', False: 'A202'},
+            'T1 - Is good credit'    : {True: 1, False: 2}
         }
 
         boolean_columns = list(inverse_boolean_map.keys())
+        
         for column in boolean_columns:
             map_column_inplace(
                 df = processed,
                 column = column,
                 value_map = inverse_boolean_map[column]
             )
+            
+        # Reverse int64 to catgegory mapping
+        processed['A8 - Installment rate in percentage of disposable income'] = processed['A8 - Installment rate in percentage of disposable income'].astype('int64')
 
         # 3. Compare statistics for each column ---------------------------------------------------------------------------------------------------------
         for col in original.columns:
 
-            ## 3.1 Case Ordinal Column (A8)
-            ## Assert value counts are equal, disregarding index
-            if col == self.ORDINAL_COLUMN:
-                orig_value_counts = original[col].value_counts().sort_values().reset_index(drop=True)
-                proc_value_counts = processed[col].value_counts().sort_values().reset_index(drop=True)
-
-                assert orig_value_counts.equals(proc_value_counts), f"Value counts for column '{col}' changed:\n{orig_value_counts}\n!=\n{proc_value_counts}"
-
-            ## Case other columns rather than A8
-            ## Compare count, unique, top, freq, mean, std, min, 25%, 50%, 75%, max as available
-            else:
                 orig_stats = original[col].describe(include='all')
                 proc_stats = processed[col].describe(include='all')
                 
--- python/src/utils/data_utils.py
+++ python/src/utils/data_utils.py
@@ -4,10 +4,9 @@ utils/data_utils.py
 Utility functions for data processing in the German Credit Data project.
 """
 import pandas as pd
-from typing import Tuple, Dict
+from typing import Dict
 
-def map_column_inplace(df: pd.DataFrame, column: Tuple[str, str], value_map: Dict) -> None:
-    """
+"""
     Map values in a DataFrame column in-place using a provided mapping.
 
     Args:
@@ -17,5 +16,39 @@ def map_column_inplace(df: pd.DataFrame, column: Tuple[str, str], value_map: Dic
 
     Returns:
         None. The DataFrame is modified in-place.
-    """
+"""
+def map_column_inplace(df: pd.DataFrame, column: str, value_map: Dict) -> None:
     df[column] = df[column].map(value_map)
+
+"""
+    Used to map the german credit data boolean columns to 
+    actual booleans.
+"""
+column_map = {
+    'A19 - Telephone': {
+        'A191': False,
+        'A192': True,
+    },
+    'A20 - Foreign worker': {
+        'A201': True,
+        'A202': False,
+    },
+    'T1 - Is good credit': {
+        1: True,
+        2: False,
+    },
+}
+
+"""
+    Ordinal column mapping."""
+# ## 3 Finally convert the ordinal column ---------------------------------------------------------------------------
+
+# ### 3.1. Define the order of your categories
+# installment_categories = [1, 2, 3, 4]
+
+# ### 3.2 Create the custom ordinal data type
+# ordinal_dtype = CategoricalDtype(categories=installment_categories, ordered=True)
+
+# ### 3.2 Apply the new data type to the column
+# ORDINAL_COLUMN = ('A8', 'Installment rate in percentage of disposable income')
+# german_credit_data[ORDINAL_COLUMN] = german_credit_data[ORDINAL_COLUMN].astype(ordinal_dtype)
